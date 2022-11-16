a9bc4f6e70c24f6aefe19f4e606f4c0774ec1fc0
jptorres454 <jptorres454@misena.edu.co>
2022-11-16 09:26:10 -0500

    a

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 #TODO
 - Crear cuerpo de la culebra 
+
+    for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
 - Garantizar como se mueva la snake
 - Controlar el movimiento de la snake 
 - Detectar colision con la comida
--- snake.py
+++ snake.py
@@ -2,21 +2,16 @@ from turtle import Screen, Turtle
 
 screen=Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor("#FF5733")
+screen.bgcolor("#34495E")
 screen.title("Snake Game Jp")
 
-starting_position = []
+starting_position = [(0,0), (-20,0), (-40,0)]
 
-segment_1 = Turtle("square")
-segment_1.color("white")
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20,0)
-
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-40,0)
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 screen.exitonclick()
